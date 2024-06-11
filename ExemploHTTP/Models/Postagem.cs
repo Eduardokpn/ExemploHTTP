a58ce94f65c08e2721a8e2e6accdf2a5197d3ce1
Your Name <eduardokaue277@gmail.com>
2024-06-10 21:28:16 -0300

    UPDATE FINISH

---
+++ ExemploHTTP/Models/Postagem.cs
@@ -0,0 +1,13 @@
+﻿namespace ExemploHTTP.Models
+{
+    public class Postagem
+    {
+        public int UserId { get; set; }
+
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ ExemploHTTP/ViewModel/PostagemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ExemploHTTP.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ExemploHTTP.ViewModel
+{
+
+    public partial class PostagemViewModel : ObservableObject
+    {
+        private readonly RestService _restService;
+
+        [ObservableProperty]
+        private ObservableCollection<Postagem> _postagens = new ObservableCollection<Postagem>();
+
+
+        public PostagemViewModel()
+        {
+            _restService = new RestService();
+            GetPostagensAsyncCommand = new Command(async () => await GetPostagensAsync());
+        }
+
+        public ICommand GetPostagensAsyncCommand { get; }
+
+        public async Task GetPostagensAsync()
+        {
+            Postagens = await _restService.GetPostagensAsync();
+        }
+    }
+
+}
